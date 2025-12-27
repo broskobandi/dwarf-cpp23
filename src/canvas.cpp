9c6248c0e60f8743b11317be6eb7f8669d3b90af
broskobandi <broskobandi@protonmail.com>
2025-12-27 23:54:10 +0000

    Add using directives to canvas.cpp

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -1,5 +1,20 @@
 module canvas;
 
+using sdl2::Renderer;
+using sdl2::Window;
+using sdl2::Texture;
+using sdl2::Color;
+using sdl2::Rect;
+using sdl2::Point;
+
+using std::vector;
+using std::filesystem::path;
+using std::size_t;
+using std::optional;
+using std::variant;
+using std::nullopt;
+using std::shared_ptr;
+
 Canvas::Canvas(const shared_ptr<Window>& win) :
 	ren(win->renderer(Renderer::Flags::PRESENTVSYNC))
 {}
